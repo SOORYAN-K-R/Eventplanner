5edae7d6dc82038d4ee518fdebdab1954dc14256
Sooryankr <129542588+Sooryankr@users.noreply.github.com>
2024-11-14 14:05:52 +0530

    3rd

--- Eventplanner/venv/Scripts/activate.bat
+++ Eventplanner/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Cloud Project\EventPlanner\venv
+set VIRTUAL_ENV=E:\Cloud\EventPlanner\Eventplanner\Eventplanner\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
