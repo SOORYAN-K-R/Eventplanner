04745deacd2c0503037e16ddbeb68964da39f917
Sooryankr <129542588+Sooryankr@users.noreply.github.com>
2024-11-18 17:41:16 +0530

    g

--- Eventplanner/app.py
+++ Eventplanner/app.py
@@ -6,6 +6,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
 from authlib.integrations.flask_client import OAuth
 import os
+import pathlib
+import os
+import pathlib
+from flask import Flask, render_template, redirect, url_for, session
+import requests
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -120,7 +132,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-
+"""
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -139,6 +151,7 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+"""
 
 @app.route('/login_facebook', methods=['POST'])
 def login_facebook():
@@ -215,6 +228,65 @@ def fbdisconnect():
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
+
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+
+
+GOOGLE_CLIENT_ID = "1049644119090-vl4j4ag7olk5j8uaoe0mtd3o896ce1hf.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="https://eventplanner-nctc.onrender.com/callback"
+)
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")
+
+
+
+
+
 
 # Run the app
 if __name__ == '__main__':
