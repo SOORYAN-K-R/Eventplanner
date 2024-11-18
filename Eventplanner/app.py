fd1ba12fb0be60c020a0f1a14866fddd45d619f0
Sooryankr <129542588+Sooryankr@users.noreply.github.com>
2024-11-18 20:02:09 +0530

    fb

--- Eventplanner/app.py
+++ Eventplanner/app.py
@@ -1,33 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
-from authlib.integrations.flask_client import OAuth
-import os
-import pathlib
-import os
-import pathlib
-from flask import Flask, render_template, redirect, url_for, session
-import requests
-from flask import Flask, session, abort, redirect, request
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
-
+from flask_login import LoginManager,login_user, logout_user
+from flask_login import login_required,current_user
 
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-from flask import g
-from flask_login import current_user
 
-@app.context_processor
-def inject_user():
-    return dict(current_user=current_user)
+
 
 # DB configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
@@ -36,7 +21,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Event Model
+#Event Model
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
@@ -44,20 +29,25 @@ class Event(db.Model):
     location = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text, nullable=False)
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    creator = db.relationship('User', backref='events')
 
-# RSVP Model
+# relation to user model
+    creator = db.relationship('User', backref='events')
+   
+    
+#RSVP model
 class RSVP(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.String(20), nullable=False)  # "Yes", "No", or "Maybe"
+    
+
     user = db.relationship('User', backref='rsvps')
     event = db.relationship('Event', back_populates='rsvps')
 
 Event.rsvps = db.relationship('RSVP', order_by=RSVP.id, back_populates='event')
 
-# User Model
+#USER Model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
@@ -69,12 +59,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+    
 # Create the database tables
 with app.app_context():
     db.create_all()
 
-# Route to Home Page
+#route to Home Page
 @app.route('/')
 def home():
     events = Event.query.all()
@@ -90,34 +80,53 @@ def create_event():
         location = request.form['location']
         description = request.form['description']
 
+        # Create a new event with created_by field set to current_user's ID
         new_event = Event(name=name, date=date, location=location, description=description, created_by=current_user.id)
         db.session.add(new_event)
         db.session.commit()
 
         return redirect(url_for('home'))
+
     return render_template('create_event.html')
 
-# Event Details
+#EVENT Details
+
 @app.route('/event/<int:event_id>')
 def event_detail(event_id):
     event = Event.query.get(event_id)
     return render_template('event_detail.html', event=event)
+#RSVP
 
-# RSVP Route
 @app.route('/rsvp/<int:event_id>', methods=['POST'])
 @login_required
 def rsvp(event_id):
+    print("RSVP route accessed")  # Debugging statement
     event = Event.query.get_or_404(event_id)
+    
+    # Check if the user has already RSVPed
     existing_rsvp = RSVP.query.filter_by(user_id=current_user.id, event_id=event.id).first()
     if existing_rsvp:
-        existing_rsvp.status = request.form['status']
+        existing_rsvp.status = request.form['status']  # Update status if they have already RSVP'd
     else:
+        # Create a new RSVP entry
         new_rsvp = RSVP(user_id=current_user.id, event_id=event.id, status=request.form['status'])
         db.session.add(new_rsvp)
+
     db.session.commit()
     return redirect(url_for('event_detail', event_id=event.id))
 
-# User Registration
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+#user registeration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -132,8 +141,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-"""
-# Login
+#Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -145,163 +153,49 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html')
 
-# Logout
+#Logout
 @app.route('/logout')
-@login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-"""
 
-@app.route('/login_facebook', methods=['POST'])
-def login_facebook():
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    access_token = request.data
-    print ("access token received %s " % access_token)
+from authlib.integrations.flask_client import OAuth
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']['app_id']
-    app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
+oauth = OAuth(app)
 
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.4/me"
-    # strip expire tag from access token
-    token = result.split("&")[0]
-
-
-    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
-    data = json.loads(result)
-    login_session['provider'] = 'facebook'
-    login_session['username'] = data["name"]
-    login_session['email'] = data["email"]
-    login_session['facebook_id'] = data["id"]
-
-    # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
-    stored_token = token.split("=")[1]
-    login_session['access_token'] = stored_token
-
-    # Get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    data = json.loads(result)
-
-    login_session['picture'] = data["data"]["url"]
-
-    # see if user exists
-    user_id = getUserID(login_session['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-    login_session['user_id'] = user_id
-
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
-    flash("Now logged in as %s" % login_session['username'])
-    return output
-
-
-@app.route('/fbdisconnect')
-def fbdisconnect():
-    facebook_id = login_session['facebook_id']
-    # The access token must me included to successfully logout
-    access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'DELETE')[1]
-    return "you have been logged out"
-
-
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-
-
-GOOGLE_CLIENT_ID = "1049644119090-vl4j4ag7olk5j8uaoe0mtd3o896ce1hf.apps.googleusercontent.com"
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="https://eventplanner-nctc.onrender.com/callback"
+facebook = oauth.register(
+    name='facebook',
+    client_id='463347610056649',
+    client_secret='2e50a00e22a44c10fee75f1d56efdbc8',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    authorize_params=None,
+    access_token_url='https://graph.facebook.com/oauth/access_token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='http://localhost:5000/login/facebook/authorized',
+    client_kwargs={'scope': 'email'}
 )
+@app.route('/')
+def index():
+    user = session.get('user')  # Retrieve user info from session
+    return render_template('index.html', user=user)  # Pass user info to template
 
+@app.route('/login/facebook')
+def login_facebook():
+    redirect_uri = url_for('facebook_authorized', _external=True)
+    return facebook.authorize_redirect(redirect_uri)
 
-def login_is_required(function):
-    def wrapper(*args, **kwargs):
-        if "google_id" not in session:
-            return abort(401)  # Authorization required
-        else:
-            return function()
+@app.route('/login/facebook/authorized')
+def facebook_authorized():
+    token = facebook.authorize_access_token()
+    resp = facebook.get('https://graph.facebook.com/me?fields=id,name,email')
+    profile = resp.json()
+    
+    # Store user info in session
+    session['user'] = profile  # You can expand this based on your needs
 
-    return wrapper
+    # Redirect to the index page after login
+    return redirect(url_for('index'))
 
-
-@app.route("/login")
-def login():
-    authorization_url, state = flow.authorization_url()
-    session["state"] = state
-    return redirect(authorization_url)
-
-
-@app.route("/callback")
-def callback():
-    flow.fetch_token(authorization_response=request.url)
-
-    if not session["state"] == request.args["state"]:
-        abort(500)  # State does not match!
-
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
-
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
-    )
-
-    session["google_id"] = id_info.get("sub")
-    session["name"] = id_info.get("name")
-    return redirect("/protected_area")
-
-
-@app.route("/protected_area")
-@login_is_required
-def protected_area():
-    # Check if the user is authenticated with Google or through regular login
-    if "google_id" in session:
-        # Display the Google user's name
-        return f"Hello Google User {session['name']}! <br/><a href='/logout'><button>Logout</button></a>"
-    elif current_user.is_authenticated:
-        return redirect(url_for("login"))
-        """
-         # Display the regular logged-in user's name
-        return f"Hello {current_user.username}! <br/><a href='/logout'><button>Logout</button></a>" 
-        """
-    else:
-        return redirect(url_for("login"))
-
-
-
-# Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
+    app.run(debug=True)
